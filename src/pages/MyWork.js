137bfcde2da8d4175feeec1f657115c785be6182
Pablo T <pablotapiavasquez@gmail.com>
2020-11-18 14:20:11 -0300

    mywork

--- src/pages/MyWork.js
+++ src/pages/MyWork.js
@@ -1,11 +1,58 @@
-
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+//Images
+import athlete from '../img/athlete-small.png';
+import theracer from '../img/theracer-small.png';
+import goodtimes from '../img/goodtimes-small.png';
 
 const MyWork = () => {
     return(
-        <div className="mywork">
-            <h1>My Work</h1>
-        </div>
-    )
-}
+        <Work>
+            <Projects>
+                <h2>the athlete</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={athlete} alt="athlete"/>
+                </Link>
+            </Projects>
+            <Projects>
+                <h2>the racer</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={theracer} alt="theracer"/>
+                </Link>
+            </Projects>
+            <Projects>
+                <h2>goodtimes</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={goodtimes} alt="goodtimes"/>
+                </Link>
+            </Projects>
+        </Work>
+    )   
+};
+
+const Work = styled.div`
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 5rem 10rem;
+    h2 {
+        padding: 1rem 0rem;
+    }
+`;
+const Projects = styled.div`
+    padding-bottom: 10rem;
+    .line{
+        height: 0.5rem;
+        background: #cccccc;
+        margin-bottom: 3rem;
+    }
+    img{
+        width: 100%;
+        height:70vh;
+        object-fit: cover;
+    }
+`;
 
 export default MyWork;
